0361acb0f49da1b820409a09ef99d61fcc5c5711
Batgere <batgerelbatgerel575@gmail.com>
2024-10-25 16:20:13 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,20 @@
+# 1-b
+# 2-b
+# 3-c
+# 4-d
+# 5-c
+# 6-a
+# 7. 1-c
+# 7.2-b
+# 8-a
+# 9-a
+# 10-d
+# 11-b
+# 12-b
+# 13-array ni bolhoor element gedeg zuiliig sanah oin bairlald hadgaldag
+# holbogdson jagsaaltuud ni hadgalah butsets bagatai baidqag bas ellementuud ni ihevchlen zeregtssen bairlald baidag
+# 14-stek ni jagsaaltin zovhon neg talaas l elementuudiig oruulah ustgah bolomjtoi shugaman ogogdliin butsets ymoo.
+# 15-c
+# 16-a
+# 17-b
+# 18-c
